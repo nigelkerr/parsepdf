0f9f48b386819997d787150cb0b1322e2aad652e
Nigel Kerr <nigel.kerr@gmail.com>
2018-06-14 18:24:59 -0400

    RLE Decode added

--- src/filters.rs
+++ src/filters.rs
@@ -201,6 +201,52 @@ pub fn decode_flate(input: &Vec<u8>) -> Result<Vec<u8>, DecodingResponse> {
 
 }
 
+named!(decode_rle_literal<&[u8], Vec<u8>>,
+    do_parse!(
+        v: re_bytes_capture!(r"^([\x00-\x7f])") >>
+        w: take!((v[0][0]+1) as u8) >>
+        ( w.to_vec() )
+    )
+);
+
+named!(decode_rle_copy<&[u8], Vec<u8>>,
+        do_parse!(
+            v: one_of!([0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d, 0x9e, 0x9f, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5, 0xb6, 0xb7, 0xb8, 0xb9, 0xba, 0xbb, 0xbc, 0xbd, 0xbe, 0xbf, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda, 0xdb, 0xdc, 0xdd, 0xde, 0xdf, 0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee, 0xef, 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff].as_ref()) >>
+            w: take!(1) >>
+            ( vec![w[0]; ((257 as u16) - (v as u16)) as usize] )
+        )
+);
+
+
+named!(decode_rle_main<&[u8], Vec<u8>>,
+    do_parse!(
+        v: fold_many0!(
+            alt!(
+                complete!(decode_rle_copy) |
+                complete!(decode_rle_literal)
+            ),
+            Vec::new(),
+            |mut accum: Vec<u8>, item: Vec<u8>| {
+                accum.extend(&item);
+                accum
+            }
+        ) >>
+        tag!(b"\x80") >>
+        ( v )
+    )
+);
+
+pub fn decode_rle(input: &Vec<u8>) -> Result<Vec<u8>, DecodingResponse> {
+    match decode_rle_main(&input[..]) {
+        Ok((_, v)) => {
+            Ok(v)
+        }
+        Err(_) => {
+            Err(DecodingResponse::DecodeError)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -246,6 +292,69 @@ mod tests {
         assert_eq!(
             Ok(b"q\nBT\n36 806 Td\nET\nQ\nq 1 0 0 1 0 0 cm /Xf1 Do Q\n".to_vec()),
             decode_flate(&data[..].to_vec())
+        );
+    }
+
+    macro_rules! decode_rle_l_test {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (input, (expected_rest, expected_result)) = $value;
+                match decode_rle_literal(input) {
+                    Ok((rest, result)) => {
+                        assert_eq!((expected_rest.as_bytes(), expected_result.to_vec()),
+                            (rest, result));
+                    },
+                    _ => {
+                        assert_eq!(1, 0);
+                    }
+                }
+            }
+        )*
+        }
+    }
+
+
+
+    decode_rle_l_test! {
+        drlt1: (b"\x00bc"[..].as_bytes(), (b"c", b"b")),
+        drlt2: (b"\x01bcd"[..].as_bytes(), (b"d", b"bc")),
+        drlt3: (b"\x02bcd"[..].as_bytes(), (b"", b"bcd")),
+        drlt4: (b"\x10abcdefghijklmnopqrstuvwxyz"[..].as_bytes(), (b"rstuvwxyz", b"abcdefghijklmnopq")),
+    }
+
+
+    macro_rules! decode_rle_c_test {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (input, (expected_rest, expected_result)) = $value;
+                match decode_rle_copy(input) {
+                    Ok((rest, result)) => {
+                        assert_eq!((expected_rest.as_bytes(), expected_result.to_vec()),
+                            (rest, result));
+                    },
+                    _ => {
+                        assert_eq!(2, 0);
+                    }
+                }
+            }
+        )*
+        }
+    }
+
+    decode_rle_c_test! {
+        drct_1: (b"\xffbc"[..].as_bytes(), (b"c", b"bb")),
+        drct_2: (b"\xf0bc"[..].as_bytes(), (b"c", b"bbbbbbbbbbbbbbbbb")),
+    }
+
+    #[test]
+    fn decode_rle_test() {
+        assert_eq!(
+            Ok(b"bbc".to_vec()),
+            decode_rle(&b"\xffb\x00c\x80"[..].to_vec())
         );
     }
 }
