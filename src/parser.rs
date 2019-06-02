51d32befda9f30524db4e53d22743fc0a4a871e5
Nigel Kerr <nigel.kerr@gmail.com>
2019-06-02 12:58:23 -0400

    start on the object hier, which requires the map

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,9 @@
 use nom::{
     branch::alt, bytes::complete::*, combinator::*, error::*, sequence::*, Err, IResult, Needed,
 };
+use std::collections::HashMap;
+use std::error;
+use std::fmt;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum PdfVersion {
@@ -33,6 +36,109 @@ impl From<&[u8]> for PdfVersion {
     }
 }
 
+#[derive(Debug, PartialEq, Clone)]
+pub enum PdfObject {
+    Null,
+    Boolean(bool),
+    Integer(i64),
+    Float(f64),
+    String(Vec<u8>),
+    Name(Vec<u8>),
+    Array(Vec<PdfObject>),
+    Dictionary(NameMap),
+    Stream(NameMap, Vec<u8>),
+    IndirectReference { number: u32, generation: u16 },
+}
+
+#[derive(Debug)]
+pub enum NameMapError {
+    KeyNotPdfName,
+    OddNumberOfItemsGiven,
+}
+
+impl fmt::Display for NameMapError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            NameMapError::KeyNotPdfName => write!(f, "Give only a PdfObject::Name as the key"),
+            NameMapError::OddNumberOfItemsGiven => {
+                write!(f, "Give only an even number of items to build a map")
+            }
+        }
+    }
+}
+
+impl error::Error for NameMapError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct NameMap {
+    map: HashMap<Vec<u8>, PdfObject>,
+}
+
+impl NameMap {
+    pub fn new() -> NameMap {
+        NameMap {
+            map: HashMap::new(),
+        }
+    }
+
+    pub fn insert(
+        &mut self,
+        k: PdfObject,
+        v: PdfObject,
+    ) -> Result<Option<PdfObject>, NameMapError> {
+        match k {
+            PdfObject::Name(x) => match self.map.insert(x, v) {
+                Some(z) => Ok(Some(z)),
+                None => Ok(None),
+            },
+            _ => Err(NameMapError::KeyNotPdfName),
+        }
+    }
+
+    pub fn of(values: Vec<PdfObject>) -> Result<Option<NameMap>, NameMapError> {
+        let mut map: NameMap = NameMap::new();
+
+        if values.len() % 2 != 0 {
+            return Err(NameMapError::OddNumberOfItemsGiven);
+        }
+
+        for window in values.chunks(2) {
+            match map.insert(window[0].clone(), window[1].clone()) {
+                Ok(_) => {}
+                Err(x) => return Err(x),
+            }
+        }
+
+        Ok(Some(map))
+    }
+
+    pub fn get(&self, k: PdfObject) -> Result<Option<PdfObject>, NameMapError> {
+        match k {
+            PdfObject::Name(x) => match self.map.get(&x) {
+                Some(ref p) => Ok(Some((*p).clone())),
+                None => Ok(None),
+            },
+            _ => {
+                // we treat this as an error now: we could treat it as None-worthy...
+                Err(NameMapError::KeyNotPdfName)
+            }
+        }
+    }
+
+    pub fn names(&self) -> Vec<PdfObject> {
+        self.map
+            .keys()
+            .map(|vecu8| PdfObject::Name(vecu8.clone()))
+            .collect()
+    }
+}
+
 pub fn recognize_pdf_version(i: &[u8]) -> IResult<&[u8], PdfVersion> {
     preceded(
         tag(b"%PDF-"),
@@ -57,6 +163,7 @@ pub fn recognize_pdf_line_end(i: &[u8]) -> IResult<&[u8], &[u8]> {
     alt((tag(b"\r\n"), tag(b"\n"), tag(b"\r")))(i)
 }
 
+/// recognize a PDF ccomment as such, even though we will just discard them.
 pub fn recognize_pdf_comment(i: &[u8]) -> IResult<&[u8], &[u8]> {
     preceded(
         tag(b"%"),
@@ -79,6 +186,9 @@ pub fn recognize_pdf_header(i: &[u8]) -> IResult<&[u8], PdfVersion> {
     }
 }
 
+pub fn recognize_pdf_null(i: &[u8]) -> IResult<&[u8], PdfObject> {
+    map(tag("null"), |_| PdfObject::Null)(i)
+}
 
 #[cfg(test)]
 mod tests {
@@ -136,4 +246,85 @@ mod tests {
         hf1: (b"%PDF-3.0\r\n", nom::Err::Error((b"3.0\r\n".as_bytes(), nom::error::ErrorKind::Tag))),
     }
 
+    #[test]
+    fn name_map_test() {
+        match NameMap::new().get(PdfObject::Name(b"A"[..].to_owned())) {
+            Ok(None) => {}
+            _ => {
+                assert_eq!(3, 0);
+            }
+        }
+
+        match NameMap::of(vec![
+            PdfObject::Name(b"A"[..].to_owned()),
+            PdfObject::Name(b"B"[..].to_owned()),
+        ]) {
+            Ok(Some(n)) => match n.get(PdfObject::Name(b"A"[..].to_owned())) {
+                Ok(Some(x)) => {
+                    assert_eq!(x, PdfObject::Name(b"B"[..].to_owned()));
+                }
+                _ => {
+                    assert_eq!(4, 0);
+                }
+            },
+            _ => {
+                assert_eq!(5, 0);
+            }
+        }
+
+        match NameMap::of(vec![
+            PdfObject::Name(b"A"[..].to_owned()),
+            PdfObject::Name(b"B"[..].to_owned()),
+            PdfObject::Name(b"A"[..].to_owned()),
+            PdfObject::Name(b"C"[..].to_owned()),
+        ]) {
+            Ok(Some(n)) => {
+                match n.get(PdfObject::Name(b"A"[..].to_owned())) {
+                    Ok(Some(x)) => {
+                        assert_eq!(x, PdfObject::Name(b"C"[..].to_owned()));
+                    }
+                    _ => {
+                        assert_eq!(6, 0);
+                    }
+                }
+
+                assert_eq!(1, n.names().len());
+            }
+            _ => {
+                assert_eq!(7, 0);
+            }
+        }
+
+        match NameMap::of(vec![PdfObject::Name(b"A"[..].to_owned())]) {
+            Ok(Some(_n)) => {
+                assert_eq!(8, 0);
+            }
+            Err(_x) => {}
+            _ => {
+                assert_eq!(9, 0);
+            }
+        }
+    }
+
+    #[test]
+    fn null_test() {
+        assert_eq!(
+            Ok((b"\r\n".as_bytes(), PdfObject::Null)),
+            recognize_pdf_null(b"null\r\n")
+        );
+        assert_eq!(
+            Err(nom::Err::Error((
+                b"nu ll\r\n".as_bytes(),
+                nom::error::ErrorKind::Tag
+            ))),
+            recognize_pdf_null(b"nu ll\r\n")
+        );
+        assert_eq!(
+            Err(nom::Err::Error((
+                b"nul\r\n".as_bytes(),
+                nom::error::ErrorKind::Tag
+            ))),
+            recognize_pdf_null(b"nul\r\n")
+        );
+    }
 }
